1c4b2452274022503c934cbf899ae05477d062ea
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-09-17 02:29:28 +0530

    paddings link - (70%)

--- src/Components/Mods/Paddings.tsx
+++ src/Components/Mods/Paddings.tsx
@@ -1,14 +1,59 @@
 import { Form, Input, Row, Col } from 'antd';
-import { useEffect, useState } from 'react';
+import _ from 'lodash';
+import { useEffect, useMemo, useState } from 'react';
+import { useEditor } from '../../Hooks/Editor.hook';
+import { useHtmlWrapper } from '../../Hooks/Htmlwrapper.hook';
+import { findUniqueIdentifier } from '../../Utils/closestParent';
+import { findElementInJson } from '../../Utils/findElementInMjmlJson';
 
 const Padding = () => {
+  const [visible, setVisible] = useState(false);
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
-  const [top] = useState(0);
-  const [bottom] = useState(0);
+  const [top, setTop] = useState(0);
+  const [bottom, setBottom] = useState(0);
+  const { active } = useHtmlWrapper();
+  const { mjmlJson, setMjmlJson } = useEditor();
+  const [path, setPath] = useState('');
   useEffect(() => {
     console.log(left, right, top, bottom);
   }, [left, top, bottom, right]);
+
+  useEffect(() => {
+    if (active) {
+      const uniqueIdentifier = findUniqueIdentifier(active, active.classList);
+      if (uniqueIdentifier) {
+        let path = findElementInJson(mjmlJson, uniqueIdentifier);
+        if (path) {
+          const [, pathToElement] = path;
+          const item = _.get(mjmlJson, pathToElement.slice(1));
+          if (item.mutableProperties.includes('padding')) {
+            setVisible(true);
+            setLeft(item.attributes['padding-left']);
+            setRight(item.attributes['padding-right']);
+            setTop(item.attributes['padding-top']);
+            setBottom(item.attributes['padding-bottom']);
+          }
+        }
+      }
+    }
+  }, [active]);
+
+  const setPadding = useMemo(
+    () => (direction: string, value: string) => {
+      if (path && visible) {
+        let json = {};
+
+        if (direction) {
+          let element = _.get(mjmlJson, path);
+          element.attributes[`padding-${direction}`] = value;
+          json = _.set(mjmlJson, path, element);
+          setMjmlJson({ ...json });
+        }
+      }
+    },
+    [left, right, top, bottom]
+  );
 
   return (
     <Form.Item label="Padding :">
