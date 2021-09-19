85a78e1d0970416e41688370429d48d58c27a6ed
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-09-19 09:14:57 +0530

    remove default assigned values from docs, passing only customized properties, default properties are generated from json itself

--- src/Components/Image.tsx
+++ src/Components/Image.tsx
@@ -16,7 +16,6 @@ export const Image = () => {
   const config = {
     tagName: 'mj-image',
     attributes: {
-      ...assigned_default_values,
       'css-class': 'mjml-tag identifier-mj-image',
       src: 'https://dev.bluepie.in/dnde.png',
     },
