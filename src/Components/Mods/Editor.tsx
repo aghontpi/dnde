e686a85c7c7f1f0702e8c068d42d2b9d4b57fe1b
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-09-26 15:48:07 +0530

    cleanup
    - remove commented out code
    - remove other editors

--- src/Components/Mods/Editor.tsx
+++ src/Components/Mods/Editor.tsx
@@ -2,14 +2,11 @@ import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { Button, Popconfirm } from 'antd';
 import ED from 'ckeditor5-custom-build/build/ckeditor';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useCkeditor } from '../../Hooks/Ckeditor.hook';
 import { useEditor } from '../../Hooks/Editor.hook';
 import { useHtmlWrapper } from '../../Hooks/Htmlwrapper.hook';
-import { findClosestParent } from '../../Utils/closestParent';
-import { findElementInJson } from '../../Utils/findElementInMjmlJson';
 import { Remove } from '../../Utils/operations';
-import { columnPlaceholder } from '../Section';
 import { findTextNode } from '../../Utils/findTextNode';
 import Quill from 'quill';
 import { useQuillEditor } from '../../Hooks/Quill.hook';
@@ -18,9 +15,6 @@ export const Editor = () => {
   const { ref, isActive, x, y, delActive, delX, delY, setDelActive } = useCkeditor();
   const { mjmlJson, setMjmlJson } = useEditor();
   const { active, setActive } = useHtmlWrapper();
-  const [quill, setQuill] = useState<any>(null);
-  const [toolbar, setToolbar] = useState<any>(null);
-  const refTxt = useRef();
   const { QuillActive, quillX, quillY } = useQuillEditor();
   useEffect(() => {
     if (ref) {
@@ -31,35 +25,14 @@ export const Editor = () => {
   useEffect(() => {
     if (active && QuillActive) {
       let textNode = findTextNode(active);
-      // if (!textNode || textNode.contentEditable) {
-      //   return;
-      // }
-      refTxt.current = textNode;
-      if (refTxt.current) {
+      if (textNode) {
         // todo: fix QuillWrapper keeps wrapping up an extra div on init
-        let lquill: any = new Quill(refTxt.current, { theme: 'snow', modules: { toolbar: '#toolbarContainer' } });
-        setQuill(lquill);
-        let toolbar = lquill.getModule('toolbar');
+        let lquill: any = new Quill(textNode, { theme: 'snow', modules: { toolbar: '#toolbarContainer' } });
         lquill.editor.scroll.domNode.classList.remove('ql-editor');
-        // remove clipboard and tooltip, figout to move somewhere else
+
+        // remove clipboard and tooltip
         lquill.container.children[1].remove();
         lquill.container.children[1].remove();
-
-        // lquill.container.classList.remove('ql-container');
-        setToolbar(toolbar);
-        // toolbar.container.style.visibility = 'hidden';V
-        // const toolbarElement = toolbar.container;
-
-        // const element = toolbar?.container;
-        // const isToolbarFocused =
-        //   element && (element.contains(document.activeElement) || element === document.activeElement);
-        // if (!isToolbarFocused) {
-        //   element.style.visibility = 'hidden';
-        // }
-        // toolbarElement.style.visibility = 'visible';
-        // toolbarElement.style.position = 'absolute';
-        // toolbarElement.style.top = `${x - 270}px`; // TODO need additional calculation for edge cases
-        // toolbarElement.style.left = `${y + 400}px`;
       }
     }
   }, [active, QuillActive]);
@@ -127,19 +100,10 @@ export const Editor = () => {
               ],
             }}
             data={'placement'}
-            onReady={(editor) => {
-              console.log('', editor);
-            }}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              console.log({ event, editor, data });
-            }}
-            onBlur={(event, editor) => {
-              console.log('Blur.', editor);
-            }}
-            onFocus={(event, editor) => {
-              console.log('Focus.', editor);
-            }}
+            onReady={(editor) => {}}
+            onChange={(event, editor) => {}}
+            onBlur={(event, editor) => {}}
+            onFocus={(event, editor) => {}}
           />
         </div>
       </div>
