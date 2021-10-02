5a453e292ec487f8a870c7178c3f5bfb5c3843f8
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-10-02 18:44:45 +0530

    link font size with value

--- src/Components/Mods/FontSize.tsx
+++ src/Components/Mods/FontSize.tsx
@@ -1,4 +1,4 @@
-import { useVisibility } from '../../Hooks/Attribute.hook';
+import { useValue, useVisibility } from '../../Hooks/Attribute.hook';
 import { Form, Input } from 'antd';
 import { useEditor } from '../../Hooks/Editor.hook';
 import { ChangeEvent } from 'react';
@@ -9,6 +9,7 @@ const ATTRIBUTE = 'font-size';
 export const FontSize = () => {
   const [visible, path] = useVisibility({ attribute: ATTRIBUTE });
   const { mjmlJson, setMjmlJson } = useEditor();
+  const { getValue } = useValue({ path, visible, attribute: ATTRIBUTE });
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     let value = e.currentTarget.value;
@@ -23,7 +24,7 @@ export const FontSize = () => {
 
   return visible ? (
     <Form.Item label="FontSize">
-      <Input onChange={handleChange} />
+      <Input onChange={handleChange} value={getValue()} />
     </Form.Item>
   ) : null;
 };
