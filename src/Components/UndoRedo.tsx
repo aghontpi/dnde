70891bc02a49fb2950b44db7a0c0f5562d8199f8
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-10-16 07:54:39 +0530

    fixed position undo redo

--- src/Components/UndoRedo.tsx
+++ src/Components/UndoRedo.tsx
@@ -9,7 +9,7 @@ interface UndoRedoProps {
 
 const UndoRedo = ({ undoCallback, redoCallback }: UndoRedoProps) => {
   return (
-    <div style={{ position: 'absolute', padding: '8px', display: 'flex', flexDirection: 'column', rowGap: '4px' }}>
+    <div style={{ position: 'fixed', padding: '8px', display: 'flex', flexDirection: 'column', rowGap: '4px' }}>
       <Tooltip mouseEnterDelay={0.5} color="cyan" title="undo" placement="right">
         <Button
           disabled={UNDOREDO.isUndoEmpty()}
