0a43350d68bd754d6bbdaa8ee720a17a953c63c2
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-10-19 07:17:39 +0530

    link tag update to editor

--- src/Components/Mods/CustomInlineEditor.tsx
+++ src/Components/Mods/CustomInlineEditor.tsx
@@ -1,6 +1,6 @@
-import { Button, Popover, Select } from 'antd';
+import { Button, Input, Popover, Select } from 'antd';
 import _ from 'lodash';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useCustomEditorPosition, useCustomEditorStatus } from '../../Hooks/CustomEditor.hook';
 import { useEditor } from '../../Hooks/Editor.hook';
 import { useHtmlWrapper } from '../../Hooks/Htmlwrapper.hook';
@@ -10,6 +10,7 @@ import {
   ItalicOutlined,
   UnderlineOutlined,
   BgColorsOutlined,
+  LinkOutlined,
 } from '@ant-design/icons';
 import { InlineEditorActions } from '../../Utils/inlineEditorActions';
 import { ColorPicker } from '../ColorPicker';
@@ -18,6 +19,17 @@ import { findElementInJson } from '../../Utils/findElementInMjmlJson';
 import { useFonts } from '../../Hooks/useFonts';
 
 let r: any;
+
+const restoreSelection = () => {
+  let restoreSel = window.getSelection();
+  if (!restoreSel) {
+    return;
+  }
+  const temp = r;
+  restoreSel?.removeAllRanges();
+  restoreSel?.addRange(r);
+  r = temp;
+};
 
 const InlineEditor = () => {
   const ref = useRef<any>(null);
@@ -53,45 +65,33 @@ const InlineEditor = () => {
 
         const Event = (e: any) => {
           stateChangeCallback(editor, mjmlJson, setMjmlJson);
-
           // e.stopPropagation();
           // e.preventDefault();
           // e.target.focus();
           // return false;
         };
 
-        // const restoreSelection = () => {
-        //   let restoreSel = window.getSelection();
-        //   if (!restoreSel) {
-        //     return;
-        //   }
-        //   const temp = r;
-        //   restoreSel?.removeAllRanges();
-        //   restoreSel.addRange(r);
-        //   r = temp;
-        // };
-
-        // const onKeyUp = () => {
-        //   r = window.getSelection()?.getRangeAt(0);
-        // };
+        const onKeyUp = () => {
+          r = window.getSelection()?.getRangeAt(0);
+        };
 
         editor.addEventListener('focusout', Event, true);
-        // editor.addEventListener('keyup', onKeyUp, false);
-        // editor.addEventListener('click', onKeyUp, false);
+        editor.addEventListener('keyup', onKeyUp, false);
+        editor.addEventListener('click', onKeyUp, false);
 
         editor.classList.add('editor-active');
         editor.setAttribute('contentEditable', 'true');
         editor.setAttribute('spellcheck', 'false');
 
-        // if (r) {
-        //   restoreSelection();
-        // }
+        if (r) {
+          // restoreSelection();
+        }
 
         return () => {
           console.log('custom editor: cleaning up dynamic attributes');
           editor.removeEventListener('focusout', Event, true);
-          // editor.removeEventListener('keyup', onKeyUp, false);
-          // editor.removeEventListener('click', onKeyUp, false);
+          editor.removeEventListener('keyup', onKeyUp, false);
+          editor.removeEventListener('click', onKeyUp, false);
         };
       }
     }
@@ -132,6 +132,29 @@ const InlineEditor = () => {
           >
             <span onMouseDown={ResetEventBehaviour} onFocus={ResetEventBehaviour}>
               {i + 1}
+            </span>
+          </Select.Option>
+        ))}
+      </Select>
+      <Select
+        size="small"
+        defaultValue={'Ubuntu'}
+        dropdownStyle={{ minWidth: '18px' }}
+        style={{ fontSize: '12px' }}
+        onChange={(value: any) => {
+          InlineEditorActions(null, 'fontFamily', value);
+        }}
+        onMouseDown={ResetEventBehaviour}
+      >
+        {fontlist.map((font) => (
+          <Select.Option
+            onMouseDown={ResetEventBehaviour}
+            onFocus={ResetEventBehaviour}
+            style={{ fontSize: '12px' }}
+            value={font}
+          >
+            <span onMouseDown={ResetEventBehaviour} onFocus={ResetEventBehaviour}>
+              {font}
             </span>
           </Select.Option>
         ))}
@@ -187,31 +210,73 @@ const InlineEditor = () => {
         style={{ fontSize: '12px' }}
         size="small"
       ></Button>
-
-      <Select
-        size="small"
-        defaultValue={'Ubuntu'}
-        dropdownStyle={{ minWidth: '18px' }}
-        style={{ fontSize: '12px' }}
-        onChange={(value: any) => {
-          InlineEditorActions(null, 'fontFamily', value);
-        }}
-        onMouseDown={ResetEventBehaviour}
-      >
-        {fontlist.map((font) => (
-          <Select.Option
-            onMouseDown={ResetEventBehaviour}
-            onFocus={ResetEventBehaviour}
-            style={{ fontSize: '12px' }}
-            value={font}
-          >
-            <span onMouseDown={ResetEventBehaviour} onFocus={ResetEventBehaviour}>
-              {font}
-            </span>
-          </Select.Option>
-        ))}
-      </Select>
+      <LinkItem setLinkCallback={(e) => InlineEditorActions(e, 'link')} />
     </div>
+  );
+};
+
+interface LinkItemProps {
+  setLinkCallback: (e: any) => void;
+}
+
+const LinkItem = ({ setLinkCallback }: LinkItemProps) => {
+  const [active, setActive] = useState(false);
+  const [link, setLink] = useState('');
+  const linkRef = useRef<any>(null);
+
+  const onChange = (e: any) => {
+    setLink(e.target.value);
+  };
+
+  const onMouseDown = (e: any) => {
+    restoreSelection();
+  };
+
+  return (
+    <Popover
+      visible={active}
+      content={
+        <div style={{ display: 'flex', gap: '8px' }}>
+          <Input
+            className="inline-editor-link"
+            ref={linkRef}
+            value={link}
+            onMouseDown={onMouseDown}
+            onChange={onChange}
+            placeholder="link"
+          />
+          <Button
+            type="default"
+            onMouseDown={ResetEventBehaviour}
+            onClick={() => {
+              setActive(false);
+              restoreSelection();
+              // document.execCommand('createLink', false, 'https://google.com');
+              document.execCommand(
+                'insertHTML',
+                false,
+                '<a href="' + link + '" target="_blank">' + document.getSelection() + '</a>'
+              );
+            }}
+          >
+            create
+          </Button>
+        </div>
+      }
+      trigger="click"
+      placement="bottom"
+    >
+      <Button
+        style={{ fontSize: '12px' }}
+        onClick={(e) => {
+          ResetEventBehaviour(e);
+          setActive(!active);
+          setLink('');
+        }}
+        size="small"
+        icon={<LinkOutlined />}
+      ></Button>
+    </Popover>
   );
 };
 
@@ -240,6 +305,17 @@ const stateChangeCallback = (item: any, mjmlJson: any, setMjmlJson: any) => {
 };
 
 export const ResetEventBehaviour = (e: any) => {
+  console.log('::reset', e.target);
+  if (
+    e.target &&
+    e.target.tagName === 'INPUT' &&
+    e.target.className &&
+    e.target.className.includes('inline-editor-link')
+  ) {
+    console.log('::reset, returning');
+    return;
+  }
+  console.log('::reset->reset behaviour', e.target);
   e.preventDefault();
   e.stopPropagation();
   return false;
