5461b3045cac860ed0a9f780cebbf98cd429e6dc
Gopinath (aka) bluepie <gopinath2nr@gmail.com>
2021-10-19 10:45:06 +0530

    list page header design

--- src/Pages/List/List.tsx
+++ src/Pages/List/List.tsx
@@ -1,28 +1,61 @@
 import { NewItem, Preview } from './Preview';
-import { useAppSelector } from '../../Hooks/AppHooks';
 import './List.scss';
-import { useEffect } from 'react';
 import { useGetTemplatesQuery } from '../../Api/api';
+import { Col, Row } from 'antd';
+import Scrollbars from 'react-custom-scrollbars-2';
+import { GithubFilled } from '@ant-design/icons';
 
 const List = () => {
-  // const { templates } = useAppSelector((state) => state.templateList);
-
   const { data, isLoading, isError, isSuccess } = useGetTemplatesQuery();
 
-  console.log('::listservice', data);
-
   return (
-    <div className="template">
-      <div className="header">header</div>
-      <div className="template-list">
-        <NewItem />
-        {isSuccess && data
-          ? data.response.map((item, key) => {
-              return <Preview key={key} id={item.docRef} image={item.preview} />;
-            })
-          : null}
+    <Scrollbars autoHide={true} style={{ height: '100%' }}>
+      <div className="template">
+        <Row align="middle" justify="center" className="header">
+          <Col span={24}>
+            <Row align="middle" justify="center">
+              <Col>
+                <span className="title">Dnde</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Row align="middle" justify="center">
+              <Col>
+                <span className="subtitle">
+                  Drag and Drop Editor tailored for <b>Mails</b>
+                </span>
+              </Col>
+            </Row>
+          </Col>
+          <Col style={{ paddingTop: '24px' }} span={24}>
+            <Row align="middle" justify="center">
+              <Col className="info" span={10} style={{ textAlign: 'center' }}>
+                <a target="_blank" href="https://github.com/aghontpi/dnde">
+                  <span>
+                    All features are optimised for mails, flexible through import/export, with responsive design for all
+                    devices.
+                  </span>
+                  <br />
+                  <span>
+                    Source on Github <GithubFilled style={{ position: 'relative', top: '4px', fontSize: '32px' }} />
+                  </span>
+                </a>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+        <div className="template-list">
+          <NewItem />
+          {isSuccess && data
+            ? data.response.map((item, key) => {
+                return <Preview key={key} id={item.docRef} image={item.preview} />;
+              })
+            : null}
+        </div>
       </div>
-    </div>
+    </Scrollbars>
   );
 };
 
